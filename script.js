9869c9453c2f4c82064d7c53e48a037a2ac4072a
afatasi26 <afatasi26@gmail.com>
2021-03-06 08:44:17 -0700

    intro java

---
+++ script.js
@@ -0,0 +1,27 @@
+//Declare a variable to store the searched city
+var city="";
+// variable declaration
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
